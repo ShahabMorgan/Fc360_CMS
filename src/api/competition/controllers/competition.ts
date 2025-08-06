97ad0b958d133d931b5a16172a5f783f13c4751e
SHAHAB <mahdishjabi9@gmail.com>
2025-08-06 13:20:07 +0330

    ADD: - comptions content type

--- src/api/competition/controllers/competition.ts
+++ src/api/competition/controllers/competition.ts
@@ -2,6 +2,6 @@
  * competition controller
  */
 
-import { factories } from '@strapi/strapi'
+import {factories} from "@strapi/strapi";
 
-export default factories.createCoreController('api::competition.competition');
+export default factories.createCoreController("api::competition.competition");
